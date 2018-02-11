5099c3e7cd25628ad5e27f04a17cafd7ebce39b1
Brian Hart <bhart214@gmail.com>
2018-02-11 06:12:50 -0500

    added in training with caret

--- activity_recognition_rf_prediction.R
+++ activity_recognition_rf_prediction.R
@@ -24,4 +24,22 @@ ha_predictions <- predict(rf, test)
 # confusion matrix
 confusionMatrix(ha_predictions, test$class)
 
+# variable importance
+importance(rf)
+varImpPlot(rf)
 
+
+
+# We are going to use caret so we can pick the best number of predictors to use at each split (mtry)
+# using the train set we defined previously
+# (takes a while to run, so be patient!)
+rf_caret <- train(class ~ ., train,
+                  preProc = c('center', 'scale'), # center and scale the predictors
+                  method = 'rf',
+                  importance = TRUE)
+
+rf_caret
+# using mtry=7 give us the lowest root MSE. 
+rf_caret$bestTune
+# in out final model, rm and lstat are also the most important predictors
+varImpPlot(rf_caret$finalModel)
